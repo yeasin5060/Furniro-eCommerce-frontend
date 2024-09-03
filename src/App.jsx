8fdd29cd1535e4f09c47b6a0ce626ad49a7e31f7
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-09-04 01:38:27 +0600

    navbar design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+import Main from "./Layout/Main/Main";
+import Home from "./Pages/Home/Home";
+
 
 function App() {
  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+     <Route element ={<Main/>}>
+        <Route path="/" element = {<Home/>}/>
+     </Route>
+    )
+  );
 
   return (
     <>
-      <h1>hello</h1>
+      <RouterProvider
+      router={router}
+    />
     </>
   )
 }
---
+++ src/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import logo from '../../images/furniro logo.png';
+import { Link } from 'react-router-dom';
+import { RiAccountCircleLine } from "react-icons/ri";
+import { IoSearchOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+import { BsCart3 } from "react-icons/bs";
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav id='navbar'>
+        <div className='container'>
+            <div className='nav-wrapper'>
+                <div className='nav-logo-container'>
+                    <img src={logo} alt='not found' />
+                </div>
+                <div className='nav-pages-container'>
+                    <ul className='nav-pages-items'>
+                        <li>
+                            <Link to ='/'>home</Link>
+                        </li>
+                        <li>
+                            <Link to ='about'>about</Link>
+                        </li>
+                        <li>
+                            <Link to ='shop'>shop</Link>
+                        </li>
+                        <li>
+                            <Link to ='contact'>contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='nav-icons-container'>
+                    <div className='nav-icon-box'>
+                        <RiAccountCircleLine />
+                    </div>
+                    <div className='nav-icon-box'>
+                        <IoSearchOutline />
+                    </div>
+                    <div className='nav-icon-box'>
+                        <FaRegHeart />
+                    </div>
+                    <div className='nav-icon-box'>
+                        <BsCart3 />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
